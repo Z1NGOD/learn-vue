8e7b94189df116ce306f28bd4104082cd65aac68
Z1NGOD <YaroslavBorovskiy@gmail.com>
2024-11-05 17:31:31 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import ButtonStart from "./components/Button-start/Button-start.vue";
+</script>
+<template>
+  <ButtonStart />
+</template>
+
---
+++ src/components/Button-start/Button-start.vue
@@ -0,0 +1,3 @@
+<template>
+  <button type="button" class="">Start Working</button>
+</template>
